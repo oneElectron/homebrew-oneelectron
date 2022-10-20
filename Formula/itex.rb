948088acc9ef1b533694d574b99456ccf0fc7212
oneElectron <one.electron109@protonmail.com>
2022-10-20 20:34:29 +0200

    init

---
+++ Formula/itex.rb
@@ -0,0 +1,28 @@
+class Itex < Formula
+  desc "initialize a latex project"
+  homepage ""
+  url "https://github.com/oneElectron/itex/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "b343d2a31e7a146db852235a5bdff512d182f47616757da567fee7fb579a9d5a"
+  license ""
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+
+    system "cargo", "install", "--path", ".", "--root", "#{prefix}"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test itex`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Oneelectron Oneelectron
+
+## How do I install these formulae?
+
+`brew install oneelectron/oneelectron/<formula>`
+
+Or `brew tap oneelectron/oneelectron` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).

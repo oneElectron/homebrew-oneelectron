657bf07ddfaf232964626c22ea65d5b75c5dad43
Electron <one.electron109@protonmail.com>
2022-10-21 14:39:15 +0200

    Update itex.rb

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -6,7 +6,7 @@ class Itex < Formula
   homepage ""
   url "https://github.com/oneElectron/itex/archive/refs/tags/v0.2.0.tar.gz"
   version "0.2.0"
-  sha256 ""
+  sha256 "a8ea517f390bb1551da75e3a61b786dfbf8fe6629bc61c80924d0d0b85c1dbde"
   license "GPL"
 
   depends_on "rust" => :build

93c5992d798f12a613bc5dc75ee9e8a556e7e923
oneElectron <one.electron109@protonmail.com>
2022-11-02 11:59:09 +0100

    bump itex

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -4,8 +4,8 @@
 class Itex < Formula
   desc "A tool to start a latex project"
   homepage ""
-  url "https://github.com/oneElectron/itex/archive/refs/tags/v0.3.0.tar.gz"
-  version "0.3.0"
+  url "https://github.com/oneElectron/itex/archive/refs/tags/v0.3.2.tar.gz"
+  version "0.3.2"
   sha256 ""
   license "GPL"
 

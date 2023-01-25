41aa282e4907f72d803756aa8bf4c7d39d1f893d
Electron <one.electron109@protonmail.com>
2023-01-25 15:49:15 +0100

    Update itex.rb

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -4,8 +4,8 @@
 class Itex < Formula
   desc "A tool to start a latex project"
   homepage ""
-  url "https://github.com/oneElectron/itex/archive/refs/tags/v0.4.2.tar.gz"
-  version "0.4.2"
+  url "https://github.com/oneElectron/itex/archive/refs/tags/v0.5.0.tar.gz"
+  version "0.5.0"
   sha256 ""
   license "GPL"
 

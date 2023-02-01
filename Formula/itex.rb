61311f2b348e272b630b562e16e7bd7e1058d9c4
oneElectron <one.electron109@protonmail.com>
2023-02-01 09:34:50 +0100

    cleaned up itex

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -1,30 +1,18 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Itex < Formula
-  desc "A tool to start a latex project"
+  desc "Tool to start a latex project"
   homepage ""
   url "https://github.com/oneElectron/itex/archive/refs/tags/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 ""
-  license "GPL"
+  sha256 "eca9ec24bb44ff7ab19b31e5205a52e98324c98f3481b3b0bc06d97c9d95ce18"
+  license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system "cp", "-r", "./itex-templates", "#{prefix}/"
-    system "cargo", "install", "--path", ".", "--root", "#{prefix}"
+    cp_r("./itex-templates", "#{prefix}/")
+    system "cargo", "install", "--path", ".", "--root", prefix.to_s
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test itex`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"

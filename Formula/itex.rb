bdf594d9863597da6fa0fce3738f534d909d131f
oneElectron <one.electron109@protonmail.com>
2023-02-11 16:18:29 +0100

    upgrade itex, remove cpp-log2file

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -1,20 +1,20 @@
 class Itex < Formula
   desc "Tool to start a latex project"
   homepage ""
-  url "https://github.com/oneElectron/itex/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "cd1a4a08cade46d259f6cb98f2e5d4264971300a3475d05cc003145479f41802"
+  url "https://github.com/oneElectron/itex/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 ""
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
 
   def install
-    cp_r("./itex-templates", "#{prefix}/")
-    system "cargo", "install", "--path", ".", "--root", prefix.to_s
+    system "mkdir", "#{prefix}/etc"
+    system "mkdir", "#{prefix}/etc/itex"
+    cp_r("./itex-templates", "#{prefix}/etc/itex/itex-templates")
+    system "cargo", "install", "--no-default-features", "--path", ".", "--root", prefix.to_s
   end
 
   test do
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end

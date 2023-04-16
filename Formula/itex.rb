9ccc736c95fdf7de8eacdb7d36f8eea699806bf2
oneElectron <one.electron109@protonmail.com>
2023-04-16 17:25:08 +0200

    bump itex

--- Formula/itex.rb
+++ Formula/itex.rb
@@ -1,8 +1,8 @@
 class Itex < Formula
   desc "Tool to start a latex project"
   homepage ""
-  url "https://github.com/oneElectron/itex/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 ""
+  url "https://github.com/oneElectron/itex/archive/refs/tags/v1.3.3.tar.gz"
+  sha256 "f0ced3a110f52ff5fdec7a7af1a2ce996b747dd5dc44b4c950b0e80a1ba307ba"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
